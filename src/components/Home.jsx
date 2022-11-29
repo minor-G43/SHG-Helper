4686becb1be4c431483cc935bc98e5015592cb5d
Abhir-24 <abhir.raj2410@gmail.com>
2022-11-29 10:24:20 +0530

    setup pushed

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+const Home = () => {
+  return (
+    <div className='Home'>
+      
+      <div className="container">
+      <h1>SIH Helper <br /><br /><br /> Home Page</h1>
+      <br />
+      <div className="links-home">
+        <Link to='/signup' style={{fontWeight: 'bold'}} className='login-link'>Sign Up</Link>
+        <Link to='/login' style={{fontWeight: 'bold'}} className='login-link'>Login</Link>
+
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
