ddb90ceccdc444fbc0861618ec86c4ae933efb52
Abhir-24 <abhir.raj2410@gmail.com>
2023-03-14 13:28:17 +0530

    member accept deny done

--- src/components/Admin/Requests.jsx
+++ src/components/Admin/Requests.jsx
@@ -1,5 +1,5 @@
 import {useState,useEffect} from 'react'
-import { collection,getDocs,addDoc, setDoc, doc, getDoc, updateDoc, arrayUnion } from 'firebase/firestore';
+import { collection,getDocs,addDoc, setDoc, doc, getDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
@@ -7,7 +7,12 @@ import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import {Button} from '@mui/material';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Typography } from '@mui/material';
@@ -16,6 +21,29 @@ import { db } from '../../firebase.config';
 const Requests = () => {
     const [id,setId] = useState()
     const [fields,setFields] = useState([])
+    const [open, setOpen] = useState(false);
+    const [open2, setOpen2] = useState(false);
+    const [details,setDetails] = useState()
+
+    const handleClickAcceptOpen = (det) => {
+      setOpen(true);
+      setDetails(det)
+    };
+
+    const handleClickDenyOpen = (det) => {
+      setOpen2(true);
+      setDetails(det)
+    };
+
+    const handleAcceptClose = () => {
+      setOpen(false);
+      setDetails()
+    };
+
+    const handleDenyClose = () => {
+      setOpen2(false);
+      setDetails()
+    };
 
     useEffect(() =>{
         const fetchData = async () => {
@@ -51,6 +79,49 @@ const Requests = () => {
           border: 0,
         },
       }));
+
+      const handleAccept = async () => {
+        const reqRef = doc(db, "shg",id)
+
+        await updateDoc(reqRef, {
+          members: arrayUnion({
+            username: details.username,
+            email: details.email,
+            phoneNo: details.phoneNo,
+            aadhar: details.aadhar,
+            aadhar_doc: details.aadhar_doc
+          }),
+          requests: arrayRemove({
+            username: details.username,
+            email: details.email,
+            phoneNo: details.phoneNo,
+            aadhar: details.aadhar,
+            aadhar_doc: details.aadhar_doc
+          })
+        }).then(() => {
+          // alert("Member Requests Accepted!")
+          setOpen(false);
+          console.log("accept success")})
+        .catch(err => console.log(err))
+      }
+
+      const handleDeny = async () => {
+        const reqRef = doc(db, "shg",id)
+
+        await updateDoc(reqRef, {
+          requests: arrayRemove({
+            username: details.username,
+            email: details.email,
+            phoneNo: details.phoneNo,
+            aadhar: details.aadhar,
+            aadhar_doc: details.aadhar_doc
+          })
+        }).then(() => {
+          // alert("Member Requests Accepted!")
+          setOpen2(false);
+          console.log("deny success")})
+        .catch(err => console.log(err))
+      }
     
   return (
     <div className='Requests'>
@@ -67,6 +138,8 @@ const Requests = () => {
             <StyledTableCell align="right">Phone No.</StyledTableCell>
             <StyledTableCell align="right">Aadhar No.</StyledTableCell>
             <StyledTableCell align="right">Aadhar Document</StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -81,7 +154,9 @@ const Requests = () => {
                   </StyledTableCell>
                   <StyledTableCell align="right">{post.phoneNo}</StyledTableCell>
                   <StyledTableCell align="right">{post.aadhar}</StyledTableCell>
-                  <StyledTableCell align="right"><Button variant="outlined" href={post?.aadhar_doc} target="_blank" size='small'>Join</Button></StyledTableCell>
+                  <StyledTableCell align="right"><Button variant="outlined" href={post?.aadhar_doc} target="_blank" size='small'>View</Button></StyledTableCell>
+                  <StyledTableCell align="right"><Button variant="outlined" onClick={() => handleClickAcceptOpen(post)} color='success' size='small'>Accept</Button></StyledTableCell>
+                  <StyledTableCell align="right"><Button variant="outlined" onClick={() => handleClickDenyOpen(post)} color='error' size='small'>Deny</Button></StyledTableCell>
             {/* boolean condn. for display */}
                 </StyledTableRow>
                 )
@@ -94,6 +169,50 @@ const Requests = () => {
           {/* </div> */}
           {/* </Stack> */}
         </div>
+
+        <Dialog
+        open={open}
+        onClose={handleAcceptClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Accept Request"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+          Confirm that the user will be a member of your SHG
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleAcceptClose}>Disagree</Button>
+          <Button onClick={handleAccept} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={open2}
+        onClose={handleDenyClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Deny Request"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Confirm that the user will <span style={{fontWeight: 'bold'}}>not</span> be a member of your SHG
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDenyClose}>Disagree</Button>
+          <Button onClick={handleDeny} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
